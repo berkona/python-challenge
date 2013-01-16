eb2531662b6ef998f15714e637a515eef68932b0
Jon Monroe <berkona@gmail.com>
2013-01-16 16:50:34 -0500

    Changed to nest list comprehension cuz we're cool

--- 05.py
+++ 05.py
@@ -9,6 +9,5 @@ with urlopen(URL) as response:
 	obj = pickle.load(response)
 
 # I had to look what wtf kinda object this was... Python you assume too much
-for line in obj:
-	# Use Python's native understanding of tuples for this comprehension
-	print(''.join( ch * count for ch, count in line))
+# Use Python's native understanding of tuples for this comprehension
+print(''.join( ch * count for ch, count in ( line for line in obj ) ))
