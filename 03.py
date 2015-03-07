120fdcdcbf3d968c60231d8a779351fbc19a1ebe
Jon Monroe <solipsis.dev@gmail.com>
2015-03-07 05:37:23 -0500

    New 03 solve

--- 03.py
+++ 03.py
@@ -1,8 +1,7 @@
 import re
 
-regex = re.compile('(?<![A-Z])[A-Z]{3}([a-z])[A-Z]{3}(?![A-Z])')
+regex = re.compile('[^A-Z][A-Z]{3}([a-z])[A-Z]{3}[^A-Z]')
 # Remember kids, always close your file-like objects
 with open('03_data.txt') as kruft:
 	contents = kruft.read().replace('\n', '')
-matches = regex.findall(contents)
-print('matches:', ' '.join(matches))
+print ''.join(regex.findall(contents))
